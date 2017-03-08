0bb1b63c301605dbe6b185f17413c674f6d0fb9d
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2017-03-08 09:57:11 +0530

    Created global module

--- cli.js
+++ cli.js
@@ -1,9 +1,9 @@
+#!/usr/bin/env node
+
 const argv = require('yargs').argv;
 const axios = require('axios');
 
 const [ firstPackage, secondPackage ] = argv._;
 
-const getPackage = package => {
-  const url = ``;
-  return axios.get(url);
-}
+console.log('You are comparing', firstPackage, secondPackage);
+

5c115f95b132e8db24e51594c528949506d718e0
Sanchit Nevgi <s.nevgi@samsung.com>
2017-12-28 09:28:37 +0530

    Format source with prettier, Update yarn.lock

--- cli.js
+++ cli.js
@@ -9,107 +9,132 @@ const packageNames = argv._;
 
 const getPackageDetails = name => {
   const url = `https://api.npms.io/v2/package/${name}`;
-  return axios.get(url)
+  return axios
+    .get(url)
     .then(res => {
-      if(res.status !== 200) return Promise.reject(res.data.message);
+      if (res.status !== 200) return Promise.reject(res.data.message);
       return res.data;
     })
     .then(mapResponseToPackage)
     .catch(err => {
       console.log('Could not fetch package details!');
     });
-}
+};
 
 /*
   Stats that are compared:
   name, version, description, rating, author, modified, downloads, stars, issues, repository, dependencies
 */
 const mapResponseToPackage = response => {
-  
-  const { metadata: { name, version = '', description = '', author = { name: '' }, date, links = { repository: '' }, dependencies = {} },
-          npm = { downloads: 0 }, github = { starsCount: 0, issues: { openCount: 0 } } } = response.collected;
+  const {
+    metadata: {
+      name,
+      version = '',
+      description = '',
+      author = { name: '' },
+      date,
+      links = { repository: '' },
+      dependencies = {},
+    },
+    npm = { downloads: 0 },
+    github = { starsCount: 0, issues: { openCount: 0 } },
+  } = response.collected;
 
-  
-  const [ daily = 0, weekly = 0, monthly = 0 ] = npm.downloads.map(data => data.count);
+  const [daily = 0, weekly = 0, monthly = 0] = npm.downloads.map(
+    data => data.count,
+  );
 
-  const package = { name, version, description, modified: distanceInWordsToNow(date), author: author.name,
-                    repository: links.repository, dependencies: Object.keys(dependencies).length,
-                    stars: github.starsCount, issues: github.issues.openCount,
-                    daily, weekly, monthly, rating: formatRating(response.score.final) };
+  const package = {
+    name,
+    version,
+    description,
+    modified: distanceInWordsToNow(date),
+    author: author.name,
+    repository: links.repository,
+    dependencies: Object.keys(dependencies).length,
+    stars: github.starsCount,
+    issues: github.issues.openCount,
+    daily,
+    weekly,
+    monthly,
+    rating: formatRating(response.score.final),
+  };
 
   return package;
-}
+};
 
-const formatRating = rating => parseFloat(Math.round(rating*1000)/100).toFixed(2);
+const formatRating = rating =>
+  parseFloat(Math.round(rating * 1000) / 100).toFixed(2);
 
-const printTable = (packages) => {
-  
+const printTable = packages => {
   const table = new Table();
-  
+
   const keys = Object.keys(packages[0]);
-  
-  keys.forEach(function(key, index){
-    if(index === 0){
-      table.push({ [chalk.red(key.toUpperCase())]: packages.map(package => chalk.blue(package[key])) })
+
+  keys.forEach(function(key, index) {
+    if (index === 0) {
+      table.push({
+        [chalk.red(key.toUpperCase())]: packages.map(package =>
+          chalk.blue(package[key]),
+        ),
+      });
     } else {
       let counter = 0;
       let highest = undefined;
       let lowest = undefined;
-      packages.map(function(package, index){
-        if(['stars', 'daily', 'weekly', 'monthly', 'rating'].includes(key)){
-          if(highest === undefined){
+      packages.map(function(package, index) {
+        if (['stars', 'daily', 'weekly', 'monthly', 'rating'].includes(key)) {
+          if (highest === undefined) {
             highest = 0;
           }
-          if (package[key] > packages[highest][key]){
+          if (package[key] > packages[highest][key]) {
             highest = index;
           }
         }
-        if(['dependencies', 'issues'].includes(key)){
-          if(lowest === undefined){
+        if (['dependencies', 'issues'].includes(key)) {
+          if (lowest === undefined) {
             lowest = 0;
           }
-          if (package[key] <= packages[lowest][key]){
+          if (package[key] <= packages[lowest][key]) {
             lowest = index;
           }
         }
         return package[key];
       });
-      table.push({ [chalk.red(key.toUpperCase())]: packages.map(function(package, index){
-        if(['issues', 'stars', 'daily', 'weekly', 'monthly'].includes(key)){
-          package[key] = package[key].toLocaleString('en');
-        }
-        if(highest !== undefined){
-          if(index === highest || package[key] === packages[highest][key]){
-            return chalk.green(package[key])
-          } else {
-            return chalk.red(package[key])
+      table.push({
+        [chalk.red(key.toUpperCase())]: packages.map(function(package, index) {
+          if (['issues', 'stars', 'daily', 'weekly', 'monthly'].includes(key)) {
+            package[key] = package[key].toLocaleString('en');
           }
-        } else if(lowest !== undefined){
-          if(index === lowest || package[key] === packages[lowest][key]){
-            return chalk.green(package[key])
+          if (highest !== undefined) {
+            if (index === highest || package[key] === packages[highest][key]) {
+              return chalk.green(package[key]);
+            } else {
+              return chalk.red(package[key]);
+            }
+          } else if (lowest !== undefined) {
+            if (index === lowest || package[key] === packages[lowest][key]) {
+              return chalk.green(package[key]);
+            } else {
+              return chalk.red(package[key]);
+            }
           } else {
-            return chalk.red(package[key])
+            return package[key];
           }
-        } else {
-          return package[key]
-        }
-      })
+        }),
       });
     }
   });
 
-  console.log(table.toString());  
-}
+  console.log(table.toString());
+};
 
 const init = () => {
-  
-  Promise
-  .all(packageNames.map(getPackageDetails))
-  .then(printTable)
-  .catch(err => {
-    console.log('Oops, looks like the comparison failed');
-  })
-
-}
+  Promise.all(packageNames.map(getPackageDetails))
+    .then(printTable)
+    .catch(err => {
+      console.log('Oops, looks like the comparison failed');
+    });
+};
 
 init();
